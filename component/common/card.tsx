afa5f72525c14555f14630fcdef77d74a625fd83
Dong H. Kim <ehdgus1524@gmail.com>
2022-10-27 11:47:09 +0900

    [ADD] Index Intergrated

--- component/common/card.tsx
+++ component/common/card.tsx
@@ -4,18 +4,21 @@ import Link from 'next/link';
 
 function SubCard() {
   return (
-    <div className="card lg:card-side bg-base-100 shadow-xl">
-  <figure><img src="https://placeimg.com/400/400/arch" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">구독하러 가요</h2>
-    <p>안전하고 믿을 수 있는 또타의 기사님들을 만나요</p>
-    <p>마음에 드는 기사님을 구독하고 필요할 때 언제든 택시를 이용해 보세요!</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">
-        <Link href='/reserve'>구독하러 가기</Link></button>
+    <div className="hero min-h-screen" style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}>
+    <div className="hero-overlay bg-opacity-60"></div>
+    <div className="hero-content text-center text-neutral-content">
+      <div className="max-w-md">
+        <h1 className="mb-5 text-5xl font-bold">또타</h1>
+        <p className="mb-5">또타와 함께 택시 구독 서비스를 이용해봐요!</p>
+        <Link href='/driver'>
+          <button className="btn btn-primary">택시 기사님 구독</button>
+        </Link>
+        <Link href='/subscribe'>
+          <button className="btn btn-primary">정기권 구독</button>
+        </Link>
+      </div>
     </div>
   </div>
-</div>
   )
 }
 
