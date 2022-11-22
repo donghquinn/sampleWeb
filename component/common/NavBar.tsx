be073583da93b6503bc208007a8efdc4f5c1b065
Dong H. Kim <ehdgus1524@gmail.com>
2022-11-22 10:11:06 +0900

    [CHANGED]

--- component/common/NavBar.tsx
+++ component/common/NavBar.tsx
@@ -1,8 +1,7 @@
 
-import mainLogo from '../../public/logo.png'
-import Link from 'next/link';
 import Image from 'next/image';
-import logo from '../../public/logo.png'
+import Link from 'next/link';
+import mainLogo from '../../public/logo.png';
 
 function NavBar() {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
 import Image from 'next/image';
-import mainLogo from '../public/main-logo.png'
-import SubscribeButton from '../component/common/subscribe';
-import LoginButton from '../component/common/login-button';
-import NavBar from '../component/common/NavBar';
+import mainLogo from '../public/main-logo.png';
 // import style from  '../styles/module.css'
 
 // style={{flex:1, flexDirection: 'row',alignItems: 'center', justifyContent:"center"}}
@@ -11,14 +7,14 @@ function Home() {
   
   return (
     <div>
-      <div className='logo'>
-        <Image src={mainLogo}></Image>
+
+    <div className="card  w-full max-w-sm shadow-2xl bg-base-100 main-card">
+      <div className="card-body main-card">
+      <h1 className='index-title'>또 타</h1>
       </div>
-
-
-
+      </div>
 </div>
   )
 }
 
-export default Home
+export default Home 
