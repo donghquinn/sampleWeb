f6dcb2647e93112476693039ef26c58f27e240f4
Dong H. Kim <ehdgus1524@gmail.com>
2022-11-23 15:25:44 +0900

    [REMOVED] drvier components pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.tsx", "./component/**/*.tsx"],
+  content: ["./pages/**/*.{tsx,ts}", "./component/**/*.{tsx,ts}"],
   theme: {
     container: {
       center: true
