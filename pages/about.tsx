1bae8a2e53a2bc5fb86e9900c5b7ad50a8b69de2
Dong H. Kim <ehdgus1524@gmail.com>
2022-11-24 13:20:01 +0900

    [CHANGEd]

--- pages/about.tsx
+++ pages/about.tsx
@@ -30,7 +30,6 @@ const features = [
 
 function About() {
   return (
-  
     <div className="bg-white py-24 sm:py-32 lg:py-40">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="sm:text-center">
