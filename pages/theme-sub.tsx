9e0e88c53f8eb99f8be108b99c38dbaf725078ca
Dong H. Kim <ehdgus1524@gmail.com>
2022-11-29 10:53:54 +0900

    [CHANGED]

--- pages/theme-sub.tsx
+++ pages/theme-sub.tsx
@@ -15,26 +15,30 @@ function ThemeSubscribe() {
       <p style={{ textAlign: "center" }}>
         연로하신 부모님께서 안전하게 이동하실 수 있는 교통 수단이 고민이신가요?
       </p>
-      <div className="flex flex-col content-center"></div>
-      <div className="flex justify-center "></div>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardContent>
-          <p style={{ textAlign: "center", fontWeight: "bold" }}>구독권 종류</p>
-          <ul>
-            <li style={{ textAlign: "left" }}>
-              <span style={{ fontWeight: "bold" }}>15KM:</span> 15,000 원
-            </li>
-            <li style={{ textAlign: "left" }}>
-              <span style={{ fontWeight: "bold" }}>30KM:</span> 29,000 원 +
-              500원 할인 쿠폰 3장
-            </li>
-            <li style={{ textAlign: "left" }}>
-              <span style={{ fontWeight: "bold" }}>60KM:</span> 39,000 원 +
-              500원 할인 쿠폰 6장
-            </li>
-          </ul>
-        </CardContent>
-      </Card>
+      <div className="flex flex-col content-center">
+        <div className="flex justify-center ">
+          <Card sx={{ maxWidth: 345 }}>
+            <CardContent>
+              <p style={{ textAlign: "center", fontWeight: "bold" }}>
+                구독권 종류
+              </p>
+              <ul>
+                <li style={{ textAlign: "left" }}>
+                  <span style={{ fontWeight: "bold" }}>15KM:</span> 15,000 원
+                </li>
+                <li style={{ textAlign: "left" }}>
+                  <span style={{ fontWeight: "bold" }}>30KM:</span> 29,000 원 +
+                  500원 할인 쿠폰 3장
+                </li>
+                <li style={{ textAlign: "left" }}>
+                  <span style={{ fontWeight: "bold" }}>60KM:</span> 39,000 원 +
+                  500원 할인 쿠폰 6장
+                </li>
+              </ul>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
