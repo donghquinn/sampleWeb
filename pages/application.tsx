3aa3d6cd5f4ea88d602a5dba9e250e13874d2fcc
Dong H. Kim <ehdgus1524@gmail.com>
2022-12-03 14:26:40 +0900

    [cHANGEd]

--- pages/application.tsx
+++ pages/application.tsx
@@ -1,13 +1,15 @@
-import VideoPlayer from '../component/common/video';
+import VideoPlayer from "../component/common/video";
 
 const AboutApplication = () => {
   return (
     <>
-    <div>
-      <VideoPlayer></VideoPlayer>
-    </div>
+      <div className="flex flex-col content-center">
+        <div className="flex justify-center">
+          <VideoPlayer></VideoPlayer>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default AboutApplication;
