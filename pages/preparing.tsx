3d29f576c37d61b1ce6c4f90bd1dc59358c8842a
Dong H. Kim <ehdgus1524@gmail.com>
2022-12-03 14:47:48 +0900

    [CHANGEd] favicon

--- pages/preparing.tsx
+++ pages/preparing.tsx
@@ -6,22 +6,22 @@ function Preparing() {
   return (
     <div>
       <div className="flex flex-col content-center">
-        <div className="flex flex-row justify-center">
-          <div>
-            <h1 style={{ textAlign: "center" }}>
-              어플리케이션은 현재 준비중입니다!
-            </h1>
-          </div>
-          <div>
-            <p>대신 또타가 준비중인 어플리케이션을 살짝 구경시켜드릴게요</p>
-          </div>
-          <div className="grid grid-flow-row content-center">
-            <div className="flex flex-auto justify-center"> </div>
-            <Link href="/preparing">
-              <Button>구경하러 가기</Button>
-            </Link>
-          </div>
+        <div className="flex justify-center">
+          <h1 style={{ textAlign: "center", fontWeight: "bold", margin: 50 }}>
+            어플리케이션은 현재 준비중입니다!
+          </h1>
         </div>
+      </div>
+      <div className="flex flex-col content-center">
+        <div className="flex justify-center">
+          <p>대신 또타가 준비중인 어플리케이션을 살짝 구경시켜드릴게요</p>
+        </div>
+      </div>
+      <div className="grid grid-flow-row content-center">
+        <div className="flex flex-auto justify-center"> </div>
+        <Link href="/application">
+          <Button>구경하러 가기</Button>
+        </Link>
       </div>
     </div>
   );
